702dd3c7b06e3cec395dd8dac17879bbf406bc77
Madison Simpson <madison_simpson@outlook.com>
2024-03-06 22:10:32 -0500

    TODO

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import requests
 
+"""
 response = requests.get(url="http://api.open-notify.org/iss-now.json") # End point URL
+response.raise_for_status()
+data = response.json()["iss_position"]
+lon = data["longitude"]
+lat = data["latitude"]
+
+position = (lon, lat)
+print(position)
+"""
